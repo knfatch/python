3874e66ac665c7dde3959d7cef15baabb7547f28
knfatch <knfatch@gmail.com>
2026-04-27 12:26:11 -0400

    Fixed Arithmetic for + - * and /
    All basic 4 calculations can be done inside a while loop. Needs to add an option for changing method once selected.

--- calculator.py
+++ calculator.py
@@ -13,38 +13,54 @@ def menu():
     selection = int(input("Enter Selection:\n"))
     return selection
 def add(x, y):
-    return x + y
+    result = x + y
+    return result
 def subtract(x, y):
-    return x - y
+    result = x - y
+    return result
 def multiply(x, y):
-    return x * y
+    result = x * y
+    return result
 def divide(x, y):
-    if y == 0:
-        return "Error: Division by zero is not allowed."
-    return x / y
+    result = x / y
+    return result
+#---------------Variables--------------------
+
 
 #----------------Main Program----------------
 user_selection = menu()
 while user_selection != "q" or user_selection != "quit":
-    user_selection = menu()
     if user_selection < 1 or user_selection > 4:
         print("Error Dectected: Enter A Valid Selection.")
-    elif user_selection == 1:
-        x = float(input("Enter the First Number:\n"))
-        y = float(input("Enter the Second Number:\n"))
+    elif user_selection == 1:  #Addition
+        x = float(input("Enter the First Number: Add\n"))
+        y = float(input("Enter the Second Number: Add\n"))
         result = add(x, y)
-    elif user_selection == 2:
-        x = float(input("Enter the First Number:\n"))
-        y = float(input("Enter the Second Number:\n"))
+        print("-----------------------")
+        print(f"Result: {result}")
+        print("-----------------------")
+    elif user_selection == 2:  #Subtraction
+        x = float(input("Enter the First Number: Subtract\n"))
+        y = float(input("Enter the Second Number: Subtract\n"))
         result = subtract(x, y)
-    elif user_selection == 3:
-        x = float(input("Enter the First Number:\n"))
-        y = float(input("Enter the Second Number:\n"))
+        print("-----------------------")
+        print(f"Result: {result}")
+        print("-----------------------")
+    elif user_selection == 3:  #Multiplication
+        x = float(input("Enter the First Number: Multiply\n"))
+        y = float(input("Enter the Second Number: Multiply\n"))
         result = multiply(x, y)
-    elif user_selection == 4:
-        x = float(input("Enter the First Number:\n"))
-        y = float(input("Enter the Second Number:\n"))
+        print("-----------------------")
+        print(f"Result: {result}")
+        print("-----------------------")
+    elif user_selection == 4:  #Division
+        x = float(input("Enter the First Number: Divide\n"))
+        y = float(input("Enter the Second Number (Can't Be '0'): Divide\n"))
+        while y == 0:
+            print(f"{x} cannot be divided by 0. Please enter a valid number.")
+            y = float(input("Enter the Second Number: Divide\n"))
         result = divide(x, y)
+        print("-----------------------")
+        print(f"Result: {result}")
+        print("-----------------------")
 #----------------Testing-Code----------------
-print(f"User Selection: {user_selection}")
-print(f"Result: {result}")
