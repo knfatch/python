c3b47837cb190c07887823cf6ddb3bc96b9fc762
knfatch <knfatch@gmail.com>
2026-05-14 20:38:59 -0400

    Add files via upload

---
+++ Python CLI Games/random_number_guess.py
@@ -0,0 +1,27 @@
+#-------------------Random Number Guessing Game--------------------
+#---------------------Imports----------------------
+import random
+#--------------------Game-Setup-Code-------------------------
+rnd_number = random.randint(1, 100)  #Computer picks a number
+nmb_guessed = 0
+attempts = 0
+#print(rnd_number) #Prints the number for testing purposes.
+#---------------------Game-Code-------------------------
+print("I'm thinking of a number between 1 and 100...can you guess it? ")
+while nmb_guessed == 0:
+    user_guess = int(input("Enter your guess:\n"))
+    try:
+        if user_guess == rnd_number:
+            attempts += 1
+            print(f"Congratulations, you did it! The number was {rnd_number}. Number of attempts: {attempts}.")
+            nmb_guessed += 1
+        elif (user_guess < 1) or (user_guess > 100):
+            print("Guess was out of range. Try guessing a number from 1 to 100.")
+        elif user_guess < rnd_number:
+            attempts += 1
+            print("Guess was too low. Guess higher.")
+        else:
+            attempts += 1
+            print("Guess was too high. Guess lower.")
+    except ValueError:
+        print("Wrong Input Detected! Try guessing a number:\n")
